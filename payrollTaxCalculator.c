8e14d365d0af8be99ad9d81d819a93fe84d3307c
Pedro Sartori Giorgetti <pedsar@gmail.com>
2024-10-26 14:23:02 -0300

    Improvement in terminal output

--- payrollTaxCalculator.c
+++ payrollTaxCalculator.c
@@ -19,10 +19,10 @@ float calculateINSS(float salary){
     float contributionValue = 0;
 
     float inssTable[5][4] = {
-        {0, 1412.00, 0.075, 0},
-        {1412.01, 2666.68, 0.09, 21.18},
-        {2666.69, 4000.03, 0.12, 101.18},
-        {4000.04, 7786.02, 0.14, 181.18},
+        {0, 1412.00, 7.5, 0},
+        {1412.01, 2666.68, 9, 21.18},
+        {2666.69, 4000.03, 12, 101.18},
+        {4000.04, 7786.02, 14, 181.18},
         {7786.02, 0, 908.86, 0}
     };
     printf("\n-----------------------------------------------\nINSS:");
@@ -34,9 +34,9 @@ float calculateINSS(float salary){
             break;
         }
         if(inssTable[i][0]<=salary && salary <= inssTable[i][1]){
-            printf("\n\n    Aliquot: %.2f%\n    Portion to be deducted: R$ %.2f\n",inssTable[i][2],inssTable[i][3]);
-            contributionValue = (salary * inssTable[i][2]);
-            printf("\n      %.2f * %.2f = %.2f",salary, inssTable[i][2],contributionValue);
+            printf("\n\n    Aliquot: %.0f%%\n    Portion to be deducted: R$ %.2f\n",inssTable[i][2],inssTable[i][3]);
+            contributionValue = (salary * (inssTable[i][2]/100));
+            printf("\n      %.2f * %.0f%% = %.2f",salary, inssTable[i][2],contributionValue);
             printf("\n      %.2f - %.2f = ",contributionValue,inssTable[i][3]);
             contributionValue = contributionValue - inssTable[i][3];
             printf("%.2f",contributionValue);
@@ -79,10 +79,10 @@ float calculateIRRF(float salary, int dependents){
 
     float irrfTable[5][4] = {
         {0, 2112.00, 0, 0},
-        {2112.01, 2826.65, 0.075, 158.40},
-        {2826.66, 3751.05, 0.15, 370.40},
-        {3751.06, 4664.68, 0.225, 651.73},
-        {4664.68, (salary+1), 0.275, 884.96}
+        {2112.01, 2826.65, 7.5, 158.40},
+        {2826.66, 3751.05, 15, 370.40},
+        {3751.06, 4664.68, 22.5, 651.73},
+        {4664.68, (salary+1), 27.5, 884.96}
     };
 
     printf("\n-----------------------------------------------\nIRRF:");
@@ -92,9 +92,9 @@ float calculateIRRF(float salary, int dependents){
             break;
         }
         if(irrfTable[i][0]<= salary && salary <= irrfTable[i][1]){
-            printf("\n\n    Aliquot: %.3f%\n    Portion to be deducted: R$ %.2f\n",irrfTable[i][2],irrfTable[i][3]);
-            contributionValue = (salary * irrfTable[i][2]);
-            printf("\n      %.2f * %.3f = %.2f",salary, irrfTable[i][2],contributionValue);
+            printf("\n\n    Aliquot: %.1f%%\n    Portion to be deducted: R$ %.2f\n",irrfTable[i][2],irrfTable[i][3]);
+            contributionValue = (salary * (irrfTable[i][2]/100));
+            printf("\n      %.2f * %.1f%% = %.2f",salary, irrfTable[i][2],contributionValue);
             printf("\n      %.2f - %.2f = ",contributionValue,irrfTable[i][3]);
             contributionValue = contributionValue - irrfTable[i][3];
             printf("%.2f",contributionValue);
@@ -121,34 +121,48 @@ float calculateEmployerSocialSecurityContribution(float salary, int risk, float 
 
     switch (risk){
         case 1:
-            taxPercentage = 0.268;
+            taxPercentage = 26.8;
             break;
         case 2:
-            taxPercentage = 0.278;
+            taxPercentage = 27.8;
             break;
         case 3:
-            taxPercentage = 0.288;
+            taxPercentage = 28.8;
             break;
         case 4:
-            taxPercentage = taxPercentage/100;
-            break;   
+            taxPercentage = taxPercentage;
+            break;  
+        default:
+            printf("\n *** Invalid option! ***\n\nPress 'enter' to continue!\n");
+            getchar();
+            getchar();
+            return 0; 
     }
 
-    contributionValue = salary * taxPercentage;
+    contributionValue = salary * (taxPercentage/100);
+    printf("\n-----------------------------------------------");
+    printf("\nEmployer Social Security Contribution:");
+    printf("\n\n      %.2f * %.1f%% = %.2f",salary, taxPercentage, contributionValue);
+    printf("\n\n-----------------------------------------------");
     return (contributionValue);
-    
 }
 
 
 float calculateLaborBenefits(float salary, float laborBenefits[3]){
+    
+    printf("\n-----------------------------------------------\nLabor Benefits:\n");
     //13º salary
     laborBenefits[0] = (salary/12);
+    printf("\n      %.2f / 12 = %.2f",salary,laborBenefits[0]);
     //Vacation
     laborBenefits[1] = (salary/12);
+    printf("\n      %.2f / 12 = %.2f",salary,laborBenefits[1]);
     //1/3 Vacation
     laborBenefits[2] = (laborBenefits[1]/3);
+    printf("\n      %.2f / 3 = %.2f",laborBenefits[1],laborBenefits[2]);
     //Total
     laborBenefits[3] = laborBenefits[0]+laborBenefits[1]+laborBenefits[2];
+    printf("\n\n-----------------------------------------------");
 }
 
 
@@ -206,11 +220,11 @@ int main(){
                 }
                 
                 //Calculating the IRRF
-                printf("\n-> Enter the number of dependents: ");
+                printf("\n\n-> Enter the number of dependents: ");
                 scanf("%i",&dependents);
                 contributionValue[2] = calculateIRRF((salary - contributionValue[1]),dependents);
 
-                printf("\nResults:\n    -> The contributions values are: \n         FGTS: %.2f\n        INSS: %.2f\n        IRRF: %.2f\n\n",contributionValue[0],contributionValue[1],contributionValue[2]);
+                printf("\n\nResults:\n    -> The contributions values are: \n        FGTS: $%.2f\n        INSS: $%.2f\n        IRRF: $%.2f\n\n",contributionValue[0],contributionValue[1],contributionValue[2]);
                 break;
             
             case 2:
@@ -218,38 +232,47 @@ int main(){
                 printf("\n Employer Social Security Contribution\n\n");
                 printf("\n\n To remember:\n     Salary is composed of: salary + commission + gratification + tips + labor benefits");
                 
-                printf("\n\n-> Enter the salary: ");
+                printf("\n\n -> Enter the salary: ");
                 scanf("%f",&salary);
                 
                 printf("\n -> Set the Occupational Accident Risk:\n   1 - Low(26.8%)\n   2 - Medium(27.8%)\n   3 - High(28.8%)\n   4 - Set your percentage\n  Option: ");
                 scanf("%i",&risk);
 
                 if(risk == 4){
-                    printf("\n -> Enter the percentage: ");
-                    scanf("%f",&taxPercentage);
-                }
-                if(risk != 1 && risk && 2 && risk != 3 && risk != 4){
-                    printf("\n *** Invalid option! ***\n");
-                    return 0;
+                    do{
+                        printf("\n -> Enter the percentage: ");
+                        scanf("%f",&taxPercentage);
+                        if(taxPercentage>100){
+                            printf("\n *You can't set a percentage higher than 100%%\n\n");
+                        }
+                    }while(taxPercentage>100);
                 }
 
                 inssEmploye = calculateEmployerSocialSecurityContribution(salary,risk,taxPercentage);
 
-                printf("\n\n Employer Social Security Contribution value is: %.2f\n",inssEmploye);
+                if(inssEmploye != 0){
+                    printf("\n\nResults:\n     Employer Social Security Contribution value is: $%.2f\n",inssEmploye);
+                }
                 break;
 
             case 3:
                 printf("******************************");
-                printf("\n\n -> 13º salary | vacation | 1/3 vacation\n\n");
-                printf("\n\n-> Enter the salary: ");
+                printf("\n\n 13º salary | vacation | 1/3 vacation");
+                printf("\n\n -> Enter the salary: ");
                 scanf("%f",&salary);
 
                 calculateLaborBenefits(salary, laborBenefits);
-                printf("\n  13º salary: %.2f\n  vacation: %.2f\n  1/3 vacation: %.2f\n\n  Total: %.2f\n\n",laborBenefits[0], laborBenefits[1], laborBenefits[2], laborBenefits[3]);
+                printf("\nResults:\n     13º salary: $%.2f\n     vacation: $%.2f\n     1/3 vacation: $%.2f\n\n  Total: $%.2f\n",laborBenefits[0], laborBenefits[1], laborBenefits[2], laborBenefits[3]);
                 break;
 
             case 4:
                 return 0;
+            
+            default:
+                printf("\n *** Invalid option! ***\n\nPress 'enter' to continue!\n");
+                getchar();
+                getchar();
+                break;
         }
     }
 }
